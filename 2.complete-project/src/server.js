af2453545215cf74a4fbbd876b0b84733dd49c5c
Erick Wendel <erick.workspace@gmail.com>
2024-08-01 17:32:53 -0300

    add demos
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- 2.complete-project/src/server.js
+++ 2.complete-project/src/server.js
@@ -1,3 +1,8 @@
+import { initalizeTracing } from './tracing.js'
+await initalizeTracing()
+
+import { trace, context } from '@opentelemetry/api';
+
 import Fastify from 'fastify';
 import { connect, seedDb } from './db.js'
 const PORT = 8080;
@@ -6,9 +11,10 @@ const app = Fastify({ logger: true });
 const _db = await connect()
 await seedDb(_db)
 
+
 let counter = 0
 app.get('/students', async (request, reply) => {
-
+    const span = trace.getSpan(context.active())
     ++counter;
     if (counter === 1) {
         const students = await _db('students').select('*')
@@ -22,6 +28,7 @@ app.get('/students', async (request, reply) => {
             students,
             message: "this is from the really bad response"
         }
+        span.setAttribute('http.response_payload', JSON.stringify(payload))
 
         return reply
             .status(202)
@@ -37,6 +44,7 @@ app.get('/students', async (request, reply) => {
             students,
             message: 'this is the best response'
         }
+        span.setAttribute('http.response_payload', JSON.stringify(payload))
 
         return reply.send(payload)
     }
